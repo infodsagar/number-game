8edca984469c0d8d97d973317ac26880eccb72d5
Sagar Dobariya <infodsagar@gmail.com>
2022-08-17 21:33:08 +1000

    0817

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,24 @@
-console.log('hii');
+//Imported express
+const express = require('express');
+
+//Import dotenv
+require('dotenv').config();
+
+//Created new app instance of express
+const app = new express();
+
+//Middleware to log connection
+app.use((req, res, next) => {
+  console.log('Connected on port ' + process.env.PORT);
+  next();
+});
+
+//Router
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
+
+//Server waiting for req
+app.listen(process.env.PORT, () => {
+  console.log('waiting on port ' + process.env.PORT);
+});
